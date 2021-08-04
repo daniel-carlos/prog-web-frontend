626b7f89c74465160723ed1fab76eeae164e58e6
Daniel Carlos <daniel.oliveira@itegra.com.br>
2021-08-04 10:06:43 -0300

    listar meus pedidos

---
+++ src/context/config.js
@@ -0,0 +1,18 @@
+export const orderStatus = {
+    open: {
+        title: "Aberto",
+        color: "blue",
+    },
+    "confirmed": {
+        title: "Confirmado",
+        color: "darkgoldenrod",
+    },
+    "delivered": {
+        title: "Finalizado",
+        color: "green",
+    },
+    "canceled": {
+        title: "Cancelado",
+        color: "red",
+    },
+}
--- src/screens/my_orders/index.js
+++ src/screens/my_orders/index.js
@@ -1,28 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import { api } from "../../api/backend";
 import { useStore } from '../../context/context';
+import { orderStatus } from '../../context/config';
+import moment from 'moment';
 
 function MyOrders(props) {
     const [orders, setOrders] = useState([]);
     const { dateNow } = useStore(s => s);
 
     useEffect(() => {
-        
         async function loadData() {
             const response = await api.post("order/list", {
                 status: "open"
             });
             if (response.ok) {
                 const data = response.data;
-                console.log("Orders", data);
+                setOrders(data.orders);
             }
         };
         loadData();
     }, []);
 
+    const OrderItem = ({ order }) => {
+        const date = moment(order.created_date).format("DD/MM/YYYY"); 
+        
+        return (
+            <div class="card">
+                <div class="card-body">
+                    <div className="d-flex justify-content-between">
+                        <div className="mx-1">
+                            <div className="fw-bold">Pedido</div>
+                            <div>{`#${order.order_id}`}</div>
+                        </div>
+                        <div className="mx-1">
+                            <div className="fw-bold">Status</div>
+                            <div style={{color: orderStatus[order.status].color}}>{`${orderStatus[order.status].title}`}</div>
+                        </div>
+                        <div className="mx-1">
+                            <div className="fw-bold">Data</div>
+                            <div>{`${date}`}</div>
+                        </div>
+                        <div className="mx-1">
+                            <div className="fw-bold">Produtos</div>
+                            <div>{order.products.reduce((a, b) => ({amount: a.amount + b.amount})).amount}</div>
+                        </div>
+                        <div className="mx-1">
+                            <div className="fw-bold">Total</div>
+                            <div>{`R$${order.products.reduce((a, b) => ({price: a.price + b.price})).price}`}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     return (
         <div className="container">
-            <span className="fs-2 text-danger">TODO: Histórico de pedidos</span>
+            <h2 className="fs-2">Meus Pedidos</h2>
+            {
+                orders.map((order, i) => {
+                    return (
+                        <OrderItem key={i} order={order} />
+                    )
+                })
+            }
         </div>
     );
 }
